827e945d206b0e4a8c4b2cba3b491e9069e8f93f
P0k0m1 <ESTEMILIO984@GMAIL.COM>
2026-09-10 17:24:15 +0300

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+
+  console.log(
+    'Employee Request API is running on http://localhost:3000',
+  );
+}
+
+bootstrap();
